101a49e66d923011d59e884f010a5b579947f078
ViRTYOZZZ63 <arty2004111@gmail.com>
2024-03-26 17:43:54 +0400

    Fix: navbar and svg

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo-svg use");
 window.addEventListener("scroll", () => { if (this.scrollY>1){
   navbar.classList.add("navbar-light");
+  logo.href.baseVal="img/sprite.svg#logo-1";
 }else{
   navbar.classList.remove("navbar-light");
+  logo.href.baseVal="img/sprite.svg#logo-white";
 }});
