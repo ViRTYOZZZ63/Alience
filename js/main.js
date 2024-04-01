fcfb08f04bdfb96dab48a274d8a74b3ee7bb979a
ViRTYOZZZ63 <arty2004111@gmail.com>
2024-04-01 20:00:51 +0400

    add:: menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
 window.addEventListener("scroll", () => { if (this.scrollY>1){
   navbar.classList.add("navbar-light");
   logo.href.baseVal="img/sprite.svg#logo-1";
@@ -7,3 +9,7 @@ window.addEventListener("scroll", () => { if (this.scrollY>1){
   navbar.classList.remove("navbar-light");
   logo.href.baseVal="img/sprite.svg#logo-white";
 }});
+mMenuToggle.addEventListener("click", (event) => {
+  event.preventDefault();
+  menu.classList.toggle("is-open");
+})
