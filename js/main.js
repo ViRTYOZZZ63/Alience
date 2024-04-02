fe9c761451cc9f8f8f91d7ce239715c76877eb51
ViRTYOZZZ63 <arty2004111@gmail.com>
2024-04-02 20:01:19 +0400

    FIX:: Menu

--- js/main.js
+++ js/main.js
@@ -2,14 +2,30 @@ const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
-window.addEventListener("scroll", () => { if (this.scrollY>1){
+const lightModeOn = (event)=> {
   navbar.classList.add("navbar-light");
-  logo.href.baseVal="img/sprite.svg#logo-1";
-}else{
+logo.href.baseVal="img/sprite.svg#logo-1";
+}
+const lightModeOff = (event)=> {
   navbar.classList.remove("navbar-light");
   logo.href.baseVal="img/sprite.svg#logo-white";
-}});
+}
+const openMenu = (event) => {
+menu.classList.add("is-open");
+mMenuToggle.classList.add("close-menu");
+document.body.style.overflow = "hidden"; // Запрет прокутки сайта под меню
+lightModeOn();
+}
+const closeMenu = (event) => {
+  menu.classList.remove("is-open");
+  mMenuToggle.classList.remove("close-menu");
+  document.body.style.overflow = ""; // возвращает прокутку сайта под меню
+ lightModeOff();
+  }
+  
+window.addEventListener("scroll", () => {  this.scrollY>1 ? lightModeOn() : lightModeOff();
+});
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
-  menu.classList.toggle("is-open");
+  menu.classList.contains("is-open") ? closeMenu() : openMenu();
 })
