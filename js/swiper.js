5a6e9b6a05d0780d498f5e3787c5c0adefcae253
ViRTYOZZZ63 <arty2004111@gmail.com>
2024-04-09 18:00:02 +0400

    add:: slider-features

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,5 @@
 // Снизу баганный код(
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.features-slider', {
   speed: 400,
   slidesPerView: 1,
   navigation: {
@@ -27,3 +27,26 @@ const swiper = new Swiper('.swiper', {
     },
   },
 });
+
+const swiperSteps = new Swiper(".steps-slider", {
+  speed: 400,
+  slidesPerView: 1 ,
+  navigation: {
+    nextEl: ".steps-button-next",
+    prevEl: ".steps-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+})
