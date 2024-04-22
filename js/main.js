f30f9a6d669af18d801e1deb9c0ad9f60a3a7149
ViRTYOZZZ63 <arty2004111@gmail.com>
2024-04-22 12:36:07 +0400

    fix:: modal switcher

--- js/main.js
+++ js/main.js
@@ -3,35 +3,48 @@ const logoLight = document.querySelector(".logo-logo-light");
 const logo = document.querySelector(".logo");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
-const lightModeOn = (event)=> {
+const lightModeOn = (event) => {
   navbar.classList.add("navbar-light");
   logo.style.display = "block";
   logoLight.style.display = "none";
 };
-const lightModeOff = (event)=> {
+const lightModeOff = (event) => {
   navbar.classList.remove("navbar-light");
   logo.style.display = "none";
   logoLight.style.display = "block";
 };
 const openMenu = (event) => {
-menu.classList.add("is-open");
-mMenuToggle.classList.add("close-menu");
-document.body.style.overflow = "hidden"; // Запрет прокутки сайта под меню
-lightModeOn();
+  menu.classList.add("is-open");
+  mMenuToggle.classList.add("close-menu");
+  document.body.style.overflow = "hidden"; // Запрет прокутки сайта под меню
+  lightModeOn();
 };
 const closeMenu = (event) => {
   menu.classList.remove("is-open");
   mMenuToggle.classList.remove("close-menu");
   document.body.style.overflow = ""; // возвращает прокутку сайта под меню
- lightModeOff();
- window.scrollY>1 ? lightModeOn() : lightModeOff(); // Проверка уровня прокрутки после закрытия меню
-  };
+  lightModeOff();
+  window.scrollY > 1 ? lightModeOn() : lightModeOff(); // Проверка уровня прокрутки после закрытия меню
+};
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
   menu.classList.contains("is-open") ? closeMenu() : openMenu();
 });
 
-window.addEventListener('scroll', () => {
-  window.scrollY>1 ? lightModeOn() : lightModeOff(); // Проверка уровня прокрутки
+window.addEventListener("scroll", () => {
+  window.scrollY > 1 ? lightModeOn() : lightModeOff(); // Проверка уровня прокрутки
 });
 
+const modal = document.querySelector(".modal");
+const modalToggle = document.querySelectorAll("[data-toggle=modal]");
+const modalClose = document.querySelector(".modal-close");
+modalToggle.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
+    modal.classList.add("is-open");
+  });
+});
+modalClose.addEventListener("click", (event) => {
+  event.preventDefault();
+  modal.classList.remove("is-open");
+});
